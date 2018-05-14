dd86af12f9c4f71e2eb6e4125d95cc80db2ce18e
Alexey <asdalexey@yandex.ru>
2018-05-14 23:58:59 +0300

    obsevable template form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import { filter } from 'rxjs/operators/filter';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('formRef') form;
   username = 'John';
+
+  onSunmit(value) {
+    console.log(value);
+  }
+
+  ngAfterViewInit() {
+    combineLatest(
+      this.form.valueChanges,
+      this.form.statusChanges,
+      (value, status) => ({ value, status })
+    ).pipe(filter(({ status }) => status === 'VALID'))
+     .subscribe(({ value }) => console.log(value));
+
+    // this.form.valueChanges.subscribe((e) => console.log(e));
+    // this.form.statusChanges.subscribe((e) => console.log(e));
+  }
 }
